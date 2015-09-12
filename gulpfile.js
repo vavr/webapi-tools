69501d79a975bc53872812ea6e5bfc7af9136e76
Alexander Hasselbach <alex@arigativa.ru>
2015-09-12 10:14:46 +0600

    Add travis build

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var argv = require('yargs').argv;
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
 var tsify = require('tsify');
+var zip = require('gulp-zip');
 
 var gulpReplace = require('gulp-replace');
 
@@ -11,7 +12,7 @@ function gulpIf(stream, condition, task) {
     return condition ? stream.pipe(task) : stream;
 }
 
-gulp.task('default', function() {
+gulp.task('build', function() {
     var dgisApiKey = argv["dgis-key"];
     
     var bundler = browserify({basedir: './src'})
@@ -22,4 +23,10 @@ gulp.task('default', function() {
     
     return gulpIf(stream, dgisApiKey, gulpReplace('2GIS_API_KEY', dgisApiKey))
         .pipe(gulp.dest('./build'));
+});
+ 
+gulp.task('pack', function () {
+    return gulp.src(['build/*', 'static/*', 'manifest.json'])
+        .pipe(zip('extension.zip'))
+        .pipe(gulp.dest('./'));
 });
